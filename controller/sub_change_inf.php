d1ff0f5cd96fdf77f2d66eb3f058603aa3e17fbc
bqd142 <bqd142@gmail.com>
2024-10-10 23:18:33 +0700

    he he he he he

--- controller/sub_change_inf.php
+++ controller/sub_change_inf.php
@@ -13,6 +13,16 @@ if(isset($_POST['txtsub'])){
         else {echo "<script>alert('Chưa thực hiện được');</script>";}
     }
 
-
-
+if(isset($_POST['txtupdate_id'])){
+    $data = new data_account();
+    move_uploaded_file($_FILES['txtavatar']['tmp_name'],'../upload/'.$_FILES['txtavatar']['name']);
+    $update=$data->update_profile_id($_POST['id'],$_POST['txtaddress'], $_FILES['txtavatar']['name'],$_POST['txtgender'],  $_POST['txthobby'],  $_POST['txtemail'], $_POST['txtpass']);
+    if($update){
+        echo "<script>alert('Đã cập nhật thông tin');
+            window.location.href = '../advance-admin/account_select.php';
+        </script>;";
+    } 
+    else {echo "<script>alert('Chưa thực hiện được');</script>";}
+}
+    
 ?>
--- model/control_user.php
+++ model/control_user.php
@@ -1,6 +1,19 @@
 <?php
 include "connect.php";
 class data_account{
+    public function select(){
+        global $conn;
+        $sql = "SELECT*FROM user_inf";
+        $run = mysqli_query($conn, $sql);
+        return $run;
+    }
+    public function sl_id($id){
+        global $conn;
+        $sql = "SELECT*FROM user_inf where id_user = '$id'";
+        $run = mysqli_query($conn, $sql);
+        return $run;
+    }
+    
     public function insert_account($name,$email,$pass){
         global $conn;
         $sql = "INSERT INTO user_inf(username,email,pass)
@@ -28,6 +41,22 @@ class data_account{
         $run = mysqli_query($conn, $sql);
         return $run;
     }
+    public function del_profile($id){
+        global $conn;
+        $sql = "delete from user_inf 
+                WHERE id_user = '$id' ";
+        $run = mysqli_query($conn, $sql);
+        return $run;
+    }
+    public function update_profile_id($id, $address, $avatar, $gender, $hobby, $email,$pass ){
+        global $conn;
+        $sql = "UPDATE user_inf 
+                SET address = '$address', avatar = '$avatar', gender = '$gender', hobby = '$hobby', email = '$email', pass = '$pass'
+                WHERE id_user = '$id'";
+        $run = mysqli_query($conn, $sql);
+        return $run;
+    }
+
 }
 
 ?>
